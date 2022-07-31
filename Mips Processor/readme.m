18a0b619de45916e7fb2d4f0d1e6594a238d263f
mohamedelshafie <72891326+mohamedelshafie@users.noreply.github.com>
2022-07-31 16:13:16 +0200

    Create readme.m

---
+++ Mips Processor/readme.m
@@ -0,0 +1,3 @@
+implemention of a 32-bit single-cycle microarchitecture MIPS processor based on Harvard Architecture. 
+The single-cycle microarchitecture executes an entire instruction in one cycle. 
+In other words instruction fetch, instruction decode, execute, write back, and program counter update occurs within a single clock cycle. 
